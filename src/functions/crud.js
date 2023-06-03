e159fa27a4662e62b2eafd97df0871ff8ec8957f
LennoxHB <bladlh1@student.op.ac.nz>
2023-06-04 08:10:17 +1200

    feat: create basic crud functions

---
+++ src/functions/crud.js
@@ -0,0 +1,54 @@
+import { setDoc, deleteDoc, getDoc, getDocs, updateDoc, onSnapshot } from 'firebase/firestore';
+import { useEffect, useState } from "react"
+
+export const getDocuments = async (query) => {
+    try {
+        return await getDocs(query)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+export const getDocument = async (query) => {
+    try {
+        return await getDoc(query)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+export const createDocument = async (ref, data) => {
+    try {
+        await setDoc(ref, data)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+export const deleteDocument = async (doc) => {
+    try {
+        await deleteDoc(doc)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+export const updateDocument = async (doc, data) => {
+    try {
+        await updateDoc(doc, data)
+    } catch (error) {
+        console.log(error.message)        
+    }
+}
+
+export const useOnSnapshot = (ref) => {    
+    const [data, setData] = useState([])
+    useEffect(() => {
+        const unsub = onSnapshot(ref, (shot) => {
+            setData(shot.docs.map((doc) => (
+                { ...doc.data(), id: doc.id }
+            )))
+        })
+        return () => unsub()
+    }, [])    
+    return data
+}
